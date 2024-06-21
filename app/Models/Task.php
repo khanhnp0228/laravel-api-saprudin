cf91f4b5a3750536f5be2b3e366c56a127b0b741
Khanh Nguyen <khanh@propolife.co.jp>
2024-06-21 13:06:52 +0700

    Linking users to other resources

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -12,4 +13,8 @@ class Task extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
